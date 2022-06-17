b2759969080d296f9a89bd208ef8aa61f9c84992
Paul Grenyer <paul.grenyer@gmail.com>
2022-06-17 11:35:17 +0100

    feat: smoke test

---
+++ src/tripitaka-transport-sumologic.ts
@@ -0,0 +1,13 @@
+import { Transport, TransportFactory } from 'tripitaka';
+import SumoLogger, { SumoLoggerOptions } from 'sumo-logger';
+
+const sumoLogic = (options: SumoLoggerOptions): Transport => {
+    const sumoLogger = new SumoLogger(options);
+
+    return ({ level, record }) => {
+        sumoLogger.log(record);
+        sumoLogger.flushLogs();
+    };
+};
+
+export { sumoLogic };
--- test/tripitaka-transport-sumologic.test.ts
+++ test/tripitaka-transport-sumologic.test.ts
@@ -1,5 +1,35 @@
+import { SumoLoggerOptions } from 'sumo-logger';
+import { Logger } from 'tripitaka';
+import { v4 } from 'uuid';
+import { sumoLogic } from '../src/tripitaka-transport-sumologic';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const SUMO_LOGIC_ENDPOINT = process.env.SUMO_LOGIC_ENDPOINT ?? '';
+const sourceName = 'tripitaka';
+const clientUrl = 'http://tripitaka';
+
+const options: SumoLoggerOptions = {
+    endpoint: SUMO_LOGIC_ENDPOINT,
+    sourceName: sourceName,
+    sessionKey: v4(),
+    clientUrl: clientUrl,
+    onError: () => {
+        // eslint-disable-next-line no-console
+        console.log('Sumo Error!');
+    },
+};
+
 describe('Transport Sumologic', () => {
     it('Smoke test', () => {
-        expect(true).toBeTruthy();
+        const logger = new Logger({
+            transports: [sumoLogic(options)],
+        });
+        const book = {
+            title: 'Monkey',
+            author: "Wu Ch'eng-en",
+            ISBN10: '9780140441116',
+        };
+        logger.info('Retrieved book', { book });
     });
 });
