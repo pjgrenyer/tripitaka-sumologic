5d90a3508a29bcd060b54ad882fe762b86629daf
Paul Grenyer <paul.grenyer@gmail.com>
2022-06-18 06:25:18 +0100

    feat: lets try and publish

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { sumoLogicProcessor } from './processors/sumologic-processor';
+import { sumoLogicTransport } from './transports/sumologic-transport';
+
+export { sumoLogicProcessor, sumoLogicTransport };
